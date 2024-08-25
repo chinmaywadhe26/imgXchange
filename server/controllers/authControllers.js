2347e33a4389abbf6a02f382a5d3c7ac77afc4a3
Chinmay Wadhe <chinmaywadhe26@gmail.com>
2024-08-25 07:07:23 +0530

    login controller

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,7 +1,8 @@
 const User = require("../models/User")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
-
+const {generateAccessToken} = require("../helpers/accessToken")
+const {generateRefreshToken} = require("../controllers/refreshToken")
 
 const signup = async (req, res) => {
     const {username, email, password, accountType} = req.body
@@ -31,7 +32,52 @@ const signup = async (req, res) => {
     }
 }
 const login = async(req, res)=>{
+    const {email, password} = req.body;
+    try{
+        let user = await User.findOne({
+            email
+        })
+        if(!user){
+            return res.json({
+                success: false, message: "please signup"
+            })
 
+        }
+        const comparePassword = await bcrypt.compare(password, user.password)
+        if(!comparePassword){
+            return res.json({
+                success:false,
+                message: "invalid creds"
+            })
+        }
+        const data = {
+            id: user._id,
+            accountType: user.accountType,
+            author: user.username,
+        }
+        console.log(process.env.ACCESS_TOKEN_SECRET);  // Should output your secret
+console.log(process.env.REFRESH_TOKEN_SECRET); // Should output your refresh secret
+
+        const accessToken = generateAccessToken(data)
+        const refreshToken = generateRefreshToken(data)
+
+       
+
+        return res.json({
+            success: true,
+            message:"Login successfull",
+            accessToken,
+            refreshToken,
+            role: user.accountType,
+            author: user.username,
+        })
+
+    }catch(error){
+            return res.json({
+                success: false, 
+                message: error.message
+            })
+    }
 };
 
 module.exports = {login, signup};
